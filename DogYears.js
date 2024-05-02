1f573ab167e2a0a6d458b8f08c348d490aecdd42
Priyankar <99131030+Sushitrashhhh@users.noreply.github.com>
2024-05-02 20:44:11 +0530

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,11 @@
+var myAge = 19; //bc meri age
+var earlyYears = 2; //idk bout this I'm just following it
+earlyYears *= 10.5; //2 is multiplied to 10.5 which stores value in earlyYears
+var laterYears = myAge - 2; //2 is subtracted from myAge and stores values in laterYears
+laterYears *= 4; // here 4 is been multilied to the value which is stored in laterYears
+console.log(earlyYears); // output is 21
+console.log(laterYears); // output is 68
+
+var myAgeInDogYears = earlyYears + laterYears; // earlyYears and laterYears get added and stores the value in myAgeInDogYears
+var myName = 'Priyankar'.toLowerCase(); // my name has been converted to lowercases 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`); //it is what it is, idk too
